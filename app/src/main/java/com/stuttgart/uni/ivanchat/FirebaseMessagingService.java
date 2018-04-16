0cf0aa449368fe553c3bf658002905aa2d010bd7
Ivan <ivanantunovic07@gmail.com>
2018-04-16 21:29:19 +0200

    V.2.0.8 Added Notifications

---
+++ app/src/main/java/com/stuttgart/uni/ivanchat/FirebaseMessagingService.java
@@ -0,0 +1,13 @@
+package com.stuttgart.uni.ivanchat;
+
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+
+    }
+}
